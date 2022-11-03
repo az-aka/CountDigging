22f84754ad5de84888248e9d567f64a32077782d
az_aka <oswin.kazaki@gmail.com>
2022-11-03 22:25:41 +0900

    イベント追加

--- src/main/java/countdigging/countdigging/EventListner.java
+++ src/main/java/countdigging/countdigging/EventListner.java
@@ -1,6 +1,12 @@
 package countdigging.countdigging;
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 public class EventListner implements Listener {
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e){
+
+    }
 }
